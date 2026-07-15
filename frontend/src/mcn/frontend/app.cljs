388c0a6d6cdca3daa8ab54536618730edf298d80
lh42rodriguez <124691075+musicnark@users.noreply.github.com>
2026-07-15 12:35:32 +0100

    add demo bike loading as test

--- frontend/src/mcn/frontend/app.cljs
+++ frontend/src/mcn/frontend/app.cljs
@@ -1,15 +1,31 @@
 (ns mcn.frontend.app
   (:require
-   [reagent.dom.client :as rdom]))
+   [reagent.dom.client :as rdom]
+   [reagent.core :as r]
+   [mcn.frontend.api :as api]))
 
 (defonce root (atom nil))
 
+(defonce app-state
+  (r/atom {:status "loading..."}))
+
+(defn load-random-bike! []
+  (-> (api/get-json "/bikes/random")
+      (.then #(swap! app-state assoc :bike {:bike-name (:bikeName %)
+                                            :url (:url %)}))
+      (.catch #(swap! app-state assoc :bike "API request failed"))))
+
 (defn app []
-  [:div
+  [:main
    [:h1 "MCN Bike Reviews Search"]
-   [:p "Frontend online"]])
+   [:p "Frontend online"]
+  [:section
+   [:h2 "Random Bike:"]
+   [:p (-> (:bike @app-state) :bike-name)]
+   [:a (-> (:bike @app-state) :url)]]])
 
 (defn init []
   (let [el (.getElementById js/document "app")]
     (reset! root (rdom/create-root el))
+    (load-random-bike!)
     (rdom/render @root [app])))
